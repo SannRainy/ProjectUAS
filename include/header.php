0fe5158a03a8e9a9db79ab80bc748b3784420a99
SannRainy <krisna_2205101053@mhs.unipma.ac.id>
2024-12-22 17:53:25 +0700

    add file

---
+++ include/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/style/header.css">
+    
+    <title>My Website</title>
+</head>
+<body>
+    <header>
+    <div class="Button-container">
+        <nav>
+            <ul class="nav-list">
+
+            </ul>
+        </nav>
+        </div>
+    </header>
+    <script src="js/script.js"></script>
+</body>
+</html>
