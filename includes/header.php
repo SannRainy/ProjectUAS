db3cc160b25312bfe46d8479c1b961b1e1d266e2
WhiteSNE <Hernandaw87@gmail.com>
2025-01-04 21:39:23 +0700

    updating framework

--- includes/header.php
+++ includes/header.php
@@ -4,37 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/style/header.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    
     <title>My Website</title>
 </head>
 <body>
     <header>
-    <div class="Dashboard">
-    <div class="Background">
-        <img src="asset\img\5630939.jpg" alt="Background Image" class="bg-img">
-    <div class="Button-container">
-        <nav>
-            <p>Isekai Resident</p>
-            <ul class="nav-list" style="list-style-type: none;">
-            <li><a href="home.php" class="social-button">Home</a></li>
-                <li><a href="AboutUs.php" class="social-button">About Us</a></li>
-                <li><a href="Product.php" class="social-button">Product</a></li>
-                <li><a href="Location.php" class="social-button">Location</a></li>
-                <li><a href="ContactUs.php" class="social-button">Contact Us</a></li>
-            </ul>
-        </nav>
-        </div>
-        <div class="article-container">
-            <article>
-                <div class="sd-title">
-                    <p style="font-size:18px; font-weight: Bold; color: #fff; text-align: left;">Isekai Resident</p>
-                    <h1 style="color: #fff; text-align: left">PENGALAMAN HIDUP<br>YANG TENANG DAN DAMAI</h1>
-                </div>        
-            </article>
-        </div>
+<!--Ini Untuk navbar-->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Isekai Resident</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Product</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Location</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+<div class="bg-img">
+    <div class="container container-fluid d-flex justify-content-center">
+    <img src="asset/img/bg_head.png" alt="bg_top">
     </div>
+</div>
     </header>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
