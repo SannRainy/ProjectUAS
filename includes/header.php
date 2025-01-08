6b45c4ef81afd317824da73fae0f86954125d0a5
WhiteSNE <Hernandaw87@gmail.com>
2025-01-08 17:08:58 +0700

    update navbar

--- includes/header.php
+++ includes/header.php
@@ -26,38 +26,40 @@ if (!isset($_SESSION["username"])) {
 <body>
 <header>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-transparent position-absolute w-100" style="z-index: 2; top: 0;">
-        <div class="container-fluid">
-            <a class="navbar-brand text-white" href="#" style="font-size: 1.5rem; padding-left: 1rem;">Isekai Resident</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav gap-5" style="padding-right: 3rem;">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../AboutUs.php">About us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../Product.php">Product</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../Location.php">Location</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="../ContactUs.php">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <form action="home.php" method="POST" class="d-flex navbar-nav">
-                            <button type="submit" name="logout" class="btn btn-danger">Logout</butto>
-                        </form>
-                    </li>
-                </ul>
-            </div>
+    <nav id="navbar" class="navbar navbar-expand-lg navbar-light position-absolute w-100" style="z-index: 2; top: 0;">
+    <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#" style="font-size: 1.5rem; padding-left: 1rem;">Isekai Resident</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav gap-5" style="padding-right: 3rem;">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../AboutUs.php">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../Product.php">Product</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../Location.php">Location</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../ContactUs.php">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <form action="home.php" method="POST" class="d-flex navbar-nav">
+                        <button type="submit" name="logout" class="btn btn-danger">Logout</button>
+                    </form>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
+
+
 
     <!-- Background Image with Text -->
     <div class="bg-img" style="position: relative; width: 100%; height: 100vh; overflow: hidden;">
@@ -70,5 +72,15 @@ if (!isset($_SESSION["username"])) {
 
 </header>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoFfS1DhsbEl8s1Ot6VRXCAeDl6UJImQeMpG7ljktBl4J6L" crossorigin="anonymous"></script>
+<script>
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbar = document.getElementById('navbar');
+
+    navbarToggler.addEventListener('click', function() {
+        // Menambahkan atau menghapus kelas bg-color hanya ketika collapse
+        navbar.classList.toggle('bg-color');
+    });
+</script>
+
 </body>
 </html>
